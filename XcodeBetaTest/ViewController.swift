ddaf397d8cb5ea97bf540ca7c8466b21388f890c
Kyle Kendall <kyle.kendall@domo.com>
2018-06-05 12:21:29 -0700

    Initial Commit

---
+++ XcodeBetaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeBetaTest
+//
+//  Created by Kyle Kendall on 6/5/18.
+//  Copyright © 2018 Domo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
